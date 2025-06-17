c82596d9f4aced40236504db3b7845080b449490
Prince Maharjan <maharjanxprince@gmail.com>
2025-06-17 02:49:26 +0000

    File handling wrapped

---
+++ Project/main.py
@@ -0,0 +1,8 @@
+import utility as ut
+
+# result = utility.multiply(10,20)
+result = ut.multiply(10,20)
+print(result)
+
+from utility import multiply
+print(multiply(20,2))
---
+++ Project/utility.py
@@ -0,0 +1,9 @@
+def multiply(a,b):
+    return a *b
+def divide (a, b):
+    return a /b
+
+# print(multiply(2,2))
+# print(divide(2,4))
+
+print(__name__)
